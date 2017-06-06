09c6517c1fd477b2c13a017a778f0550c1c5b6de
Jonathan Dai <nightghost5078@gmail.com>
2017-06-06 16:30:22 +0800

    Complete compile script.

--- bin/grpc_compile.sh
+++ bin/grpc_compile.sh
@@ -8,16 +8,18 @@ BUILD_DEST=./build
 
 mkdir -p ${PROTO_DEST}
 
-#protoc \
-#--plugin=protoc-gen-ts=./bin/protoc-gen-ts.js \
-#--ts_out=service=true:${PROTO_DEST}/proto \
-#-I ./proto \
-#proto/*.proto
-
 grpc_tools_node_protoc \
 --js_out=import_style=commonjs,binary:${PROTO_DEST} \
 --grpc_out=${PROTO_DEST} \
 --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+proto/*.proto
+
+cp -r ./src/proto_plugin/lib/template ./build/proto_plugin/lib
+
+protoc \
+--plugin=protoc-gen-ts=./bin/protoc-gen-ts.js \
+--ts_out=service=true:${PROTO_DEST}/proto \
+-I ./proto \
 proto/*.proto
 
 cp -r ${PROTO_DEST}/proto ${BUILD_DEST}
