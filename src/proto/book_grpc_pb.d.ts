35e9d7e69410ada4e3c82b5c4b2fcf2d514f2e1e
Jonathan Dai <nightghost5078@gmail.com>
2017-06-06 16:27:22 +0800

    Service d.ts generator done.

--- src/proto/book_grpc_pb.d.ts
+++ src/proto/book_grpc_pb.d.ts
@@ -1,25 +1,25 @@
 // package: com.book
 // file: book.proto
 
-import * as grpc from 'grpc';
+import * as grpc from "grpc";
 import * as book_pb from "./book_pb";
 
-const serialize_com_book_GetBookRequest = function (arg: book_pb.GetBookRequest): Buffer;
-const deserialize_com_book_GetBookRequest = function (buffer_arg: Uint8Array): book_pb.GetBookRequest;
-const serialize_com_book_Book = function (arg: book_pb.Book): Buffer;
-const deserialize_com_book_Book = function (buffer_arg: Uint8Array): book_pb.Book;
+const serialize_book_pb_GetBookRequest = function (arg: book_pb.GetBookRequest): Buffer;
+const deserialize_book_pb_GetBookRequest = function (buffer_arg: Uint8Array): book_pb.GetBookRequest;
+const serialize_book_pb_Book = function (arg: book_pb.Book): Buffer;
+const deserialize_book_pb_Book = function (buffer_arg: Uint8Array): book_pb.Book;
 
 export const BookServiceService = {
-    getBook: {
-        path: '/com.book.BookService/GetBook',
+    GetBook: {
+        path: "/com.book.BookService/GetBook",
         requestStream: false,
         responseStream: false,
         requestType: book_pb.GetBookRequest,
         responseType: book_pb.Book,
-        requestSerialize: serialize_com_book_GetBookRequest,
-        requestDeserialize: deserialize_com_book_GetBookRequest,
-        responseSerialize: serialize_com_book_Book,
-        responseDeserialize: deserialize_com_book_Book,
+        requestSerialize: serialize_book_pb_GetBookRequest,
+        requestDeserialize: deserialize_book_pb_GetBookRequest,
+        responseSerialize: serialize_book_pb_Book,
+        responseDeserialize: deserialize_book_pb_Book,
     },
 };
 
