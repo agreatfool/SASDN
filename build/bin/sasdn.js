a08ad3231ad12d5c90425019f51039b19298cf76
Jonathan Dai <nightghost5078@gmail.com>
2017-06-14 13:33:19 +0800

    Add command line files.

--- build/bin/sasdn.js
+++ build/bin/sasdn.js
@@ -1,1 +1,5 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const debug = require('debug')('SASDN:CLI');
 //# sourceMappingURL=sasdn.js.map
---
+++ src/bin/sasdn.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import * as handlebars from "handlebars";
+
+const debug = require('debug')('SASDN:CLI');
+
