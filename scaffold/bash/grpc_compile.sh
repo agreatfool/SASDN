fb7e1d4cdec3b843465c3128e535b86b22eaee60
冯洁 <fengjie@shinezone.com>
2017-08-03 18:18:42 +0800

    更新脚手架程序

--- scaffold/bash/grpc_compile.sh
+++ scaffold/bash/grpc_compile.sh
@@ -11,13 +11,13 @@ grpc_tools_node_protoc \
 --js_out=import_style=commonjs,binary:${PROTO_DEST} \
 --grpc_out=${PROTO_DEST} \
 --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
---proto_path=./proto_third_party \
+--proto_path=./spm_protos \
 -I ./proto \
 proto/*.proto
 
 protoc \
 --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
---proto_path=./proto_third_party \
+--proto_path=./spm_protos \
 --ts_out=:${PROTO_DEST} \
 -I ./proto \
 proto/*.proto
