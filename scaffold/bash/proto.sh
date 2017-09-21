ace3d45283078bf9238dac5fccb6d7934502a71a
Jonathan Dai <nightghost5078@gmail.com>
2017-09-21 17:41:49 +0800

    Add generating bash script.

---
+++ scaffold/bash/proto.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+BASEDIR=$(dirname "$0")
+cd ${BASEDIR}/../
+ROOT=${PWD}
+
+PROTO_ROOT=${ROOT}/proto
+PROTO_3RD=${ROOT}/spm_protos
+OUTPUT=${ROOT}/src
+
+echo "***********************"
+echo "Working dir: ${ROOT}"
+echo "Proto dir: ${PROTO_ROOT}"
+echo "Proto 3rd dir: ${PROTO_3RD}"
+echo "Output dir: ${OUTPUT}"
+
+echo "***********************"
+echo "Generate js codes ..."
+sasdn proto \
+--proto=${PROTO_ROOT} \
+--output=${OUTPUT} \
+--import=${PROTO_3RD} \
+--javascript \
+--all
+
+echo "Generate d.ts codes ..."
+sasdn proto \
+--proto=${PROTO_ROOT} \
+--output=${OUTPUT} \
+--import=${PROTO_3RD} \
+--javascript \
+--all
