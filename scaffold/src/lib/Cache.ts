774af798701934185b0d397aab83798d6945f62a
iT1000s <cobrasyq888@gmail.com>
2018-01-19 14:00:06 +0800

    finish Cache

--- scaffold/src/lib/Cache.ts
+++ scaffold/src/lib/Cache.ts
@@ -1,70 +1,115 @@
 import { BaseOrmEntity } from 'sasdn-database';
 import { DeepPartial } from 'typeorm/browser/common/DeepPartial';
 import { ObjectType } from 'typeorm';
+import { Logger } from './Logger';
+import {
+  SetRequest,
+  GetRequest,
+  DelRequest,
+} from '../proto/memcached/memcached_pb';
+import MSMemcachedClient from '../clients/memcached/MSMemcachedClient';
+
 export namespace Cache {
-  export async function cacheSet<T extends BaseOrmEntity>(entity: BaseOrmEntity, expire?: number): Promise<boolean> {
+  export async function setSingle(entity: BaseOrmEntity, expire?: number): Promise<boolean> {
     expire = expire || 25200;
-    if(!entity.hasId()) {
+    if (!entity.hasId()) {
+      Logger.instance.logger.warn(`Cache set failed, ${entity.constructor.name} has not given primary key`);
       return false;
     }
-    const cacheKey = `${entity.constructor.name}_${(entity.constructor as any).getId(entity)}`;
-    // todo store cache by cacheKey
-    console.log('cacheKey = ', cacheKey);
-    return true;
+    const cacheKey = `${entity.constructor.name}:${(entity.constructor as any).getId(entity)}`;
+    const setReq = new SetRequest();
+    setReq.setKey(cacheKey);
+    setReq.setValue(JSON.stringify(entity));
+    setReq.setLifeTime(expire);
+
+    const client = new MSMemcachedClient();
+    return (await client.memSet(setReq)).getResult();
   }
 
-  export async function cacheGet<T extends BaseOrmEntity>(Entity: ObjectType<T>, options: DeepPartial<T>): Promise<T | undefined> {
+  export async function getSingle<T extends BaseOrmEntity>(Entity: ObjectType<T>, options: DeepPartial<T>): Promise<T | boolean> {
     const entity = (Entity as any).create(options);
-    if(!entity.hasId()) {
-      console.log('need primary key');
-    }
-
-    const primaryKey = (Entity as any).getId(entity);
-    const cacheKey = `${Entity.name}_${primaryKey}`;
-    // todo find cache by cacheKey
-    return undefined;
-  }
-
-  export async function cacheDel<T extends BaseOrmEntity>(Entity: ObjectType<T>, options: DeepPartial<T>): Promise<boolean> {
-    const entity = (Entity as any).create(options);
-    if(!entity.hasId()) {
+    if (!entity.hasId()) {
+      Logger.instance.logger.warn(`Cache get failed, ${Entity.name} has not given primary key`);
       return false;
     }
 
     const primaryKey = (Entity as any).getId(entity);
-    const cacheKey = `${Entity.name}_${primaryKey}`;
+    const cacheKey = `${Entity.name}:${primaryKey}`;
+    const getReq = new GetRequest();
+    getReq.setKey(cacheKey);
 
-    // todo delete cache by cacheKey
-    return true;
+    const client = new MSMemcachedClient();
+    const result = (await client.memGet(getReq)).getResult();
+    if (result) {
+      const parse = JSON.parse(result);
+      if(parse) {
+        return (Entity as any).create(parse) as T;
+      }
+    }
+    return false;
   }
 
-  export async function cacheSetMulti(entities: BaseOrmEntity[], key?: string, expire?: number): Promise<boolean> {
+  export async function delSingle<T extends BaseOrmEntity>(Entity: ObjectType<T>, options: DeepPartial<T>): Promise<boolean> {
+    const entity = (Entity as any).create(options);
+    if (!entity.hasId()) {
+      Logger.instance.logger.warn(`Cache delete failed, ${Entity.name} has not given primary key`);
+      return false;
+    }
+
+    const primaryKey = (Entity as any).getId(entity);
+    const cacheKey = `${Entity.name}:${primaryKey}`;
+
+    const delReq = new DelRequest();
+    delReq.setKey(cacheKey);
+
+    const client = new MSMemcachedClient();
+    return (await client.memDel(delReq)).getResult();
+  }
+
+  export async function setMulti(entities: BaseOrmEntity[], expire?: number): Promise<boolean> {
     expire = expire || 25200;
     // means to package as one cache
-    if(key) {
-      const stringify = JSON.stringify(entities);
-      const totalLength = key.length + stringify.length;
-      // max length 1024
-      if(totalLength > 900) {
-        return false;
-      }
-      // todo delete cache by key
-    } else {
-      for(const entity of entities) {
-        cacheSet(entity, expire);
-      }
+    for (const entity of entities) {
+      setSingle(entity, expire).catch(_=>_);
     }
 
     return true;
   }
 
-  export async function cacheGetMutli<T extends BaseOrmEntity>(key: string, Entity: ObjectType<T>): Promise<T[]> {
-    // todo found cache from memcached
-    const foundCache = 'xxx';
+  export async function setMultiWithKey(entities: BaseOrmEntity[], key: string, expire?: number): Promise<boolean> {
+    expire = expire || 25200;
+    // means to package as one cache
+    const stringify = JSON.stringify(entities);
+    const totalLength = key.length + stringify.length;
+    // max length 1024
+    if (totalLength > 900) {
+      Logger.instance.logger.warn(`Cache setMultiWithKey failed, exceed maximum length`);
+      return false;
+    }
+
+    const setReq = new SetRequest();
+    setReq.setKey(key);
+    setReq.setValue(stringify);
+    setReq.setLifeTime(expire);
+
+    const client = new MSMemcachedClient();
+    return (await client.memSet(setReq)).getResult();
+  }
+
+  export async function getMutliWithKey<T extends BaseOrmEntity>(Entity: ObjectType<T>, key: string): Promise<T[]> {
+    const getReq = new GetRequest();
+    getReq.setKey(key);
+
+    const client = new MSMemcachedClient();
+    const memResult = await client.memGet(getReq);
+    const foundCache = memResult.getResult();
+    if(!foundCache) {
+      return [];
+    }
     const result: T[] = [];
     const parseList = JSON.parse(foundCache);
-    if(parseList) {
-      for(const entity of parseList) {
+    if (parseList) {
+      for (const entity of parseList) {
         const constructor = (Entity as any).create(entity);
         parseList.push(constructor as T);
       }
---
+++ scaffold/src/proto/memcached/memcached_pb.d.ts
@@ -0,0 +1,279 @@
+// package: memcached
+// file: memcached/memcached.proto
+
+import * as jspb from "google-protobuf";
+
+export class SetRequest extends jspb.Message { 
+    getKey(): string;
+    setKey(value: string): void;
+
+    getValue(): string;
+    setValue(value: string): void;
+
+    getLifeTime(): number;
+    setLifeTime(value: number): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): SetRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: SetRequest): SetRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: SetRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): SetRequest;
+    static deserializeBinaryFromReader(message: SetRequest, reader: jspb.BinaryReader): SetRequest;
+}
+
+export namespace SetRequest {
+    export type AsObject = {
+        key: string,
+        value: string,
+        lifeTime: number,
+    }
+}
+
+export class SetMultiRequest extends jspb.Message { 
+    clearMultiSetsList(): void;
+    getMultiSetsList(): Array<SetRequest>;
+    setMultiSetsList(value: Array<SetRequest>): void;
+    addMultiSets(value?: SetRequest, index?: number): SetRequest;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): SetMultiRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: SetMultiRequest): SetMultiRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: SetMultiRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): SetMultiRequest;
+    static deserializeBinaryFromReader(message: SetMultiRequest, reader: jspb.BinaryReader): SetMultiRequest;
+}
+
+export namespace SetMultiRequest {
+    export type AsObject = {
+        multiSetsList: Array<SetRequest.AsObject>,
+    }
+}
+
+export class GetRequest extends jspb.Message { 
+    getKey(): string;
+    setKey(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: GetRequest): GetRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetRequest;
+    static deserializeBinaryFromReader(message: GetRequest, reader: jspb.BinaryReader): GetRequest;
+}
+
+export namespace GetRequest {
+    export type AsObject = {
+        key: string,
+    }
+}
+
+export class GetMultiRequest extends jspb.Message { 
+    clearKeysList(): void;
+    getKeysList(): Array<string>;
+    setKeysList(value: Array<string>): void;
+    addKeys(value: string, index?: number): string;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetMultiRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: GetMultiRequest): GetMultiRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetMultiRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetMultiRequest;
+    static deserializeBinaryFromReader(message: GetMultiRequest, reader: jspb.BinaryReader): GetMultiRequest;
+}
+
+export namespace GetMultiRequest {
+    export type AsObject = {
+        keysList: Array<string>,
+    }
+}
+
+export class AddRequest extends jspb.Message { 
+    getKey(): string;
+    setKey(value: string): void;
+
+    getValue(): string;
+    setValue(value: string): void;
+
+    getLifeTime(): number;
+    setLifeTime(value: number): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): AddRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: AddRequest): AddRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: AddRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): AddRequest;
+    static deserializeBinaryFromReader(message: AddRequest, reader: jspb.BinaryReader): AddRequest;
+}
+
+export namespace AddRequest {
+    export type AsObject = {
+        key: string,
+        value: string,
+        lifeTime: number,
+    }
+}
+
+export class DelRequest extends jspb.Message { 
+    getKey(): string;
+    setKey(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): DelRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: DelRequest): DelRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: DelRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): DelRequest;
+    static deserializeBinaryFromReader(message: DelRequest, reader: jspb.BinaryReader): DelRequest;
+}
+
+export namespace DelRequest {
+    export type AsObject = {
+        key: string,
+    }
+}
+
+export class BoolResponse extends jspb.Message { 
+    getError(): string;
+    setError(value: string): void;
+
+    getResult(): boolean;
+    setResult(value: boolean): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): BoolResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: BoolResponse): BoolResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: BoolResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): BoolResponse;
+    static deserializeBinaryFromReader(message: BoolResponse, reader: jspb.BinaryReader): BoolResponse;
+}
+
+export namespace BoolResponse {
+    export type AsObject = {
+        error: string,
+        result: boolean,
+    }
+}
+
+export class BoolsResponse extends jspb.Message { 
+    getError(): string;
+    setError(value: string): void;
+
+    clearResultList(): void;
+    getResultList(): Array<boolean>;
+    setResultList(value: Array<boolean>): void;
+    addResult(value: boolean, index?: number): boolean;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): BoolsResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: BoolsResponse): BoolsResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: BoolsResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): BoolsResponse;
+    static deserializeBinaryFromReader(message: BoolsResponse, reader: jspb.BinaryReader): BoolsResponse;
+}
+
+export namespace BoolsResponse {
+    export type AsObject = {
+        error: string,
+        resultList: Array<boolean>,
+    }
+}
+
+export class DataResponse extends jspb.Message { 
+    getError(): string;
+    setError(value: string): void;
+
+    getResult(): string;
+    setResult(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): DataResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: DataResponse): DataResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: DataResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): DataResponse;
+    static deserializeBinaryFromReader(message: DataResponse, reader: jspb.BinaryReader): DataResponse;
+}
+
+export namespace DataResponse {
+    export type AsObject = {
+        error: string,
+        result: string,
+    }
+}
+
+export class MapResult extends jspb.Message { 
+    getKey(): string;
+    setKey(value: string): void;
+
+    getValue(): string;
+    setValue(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): MapResult.AsObject;
+    static toObject(includeInstance: boolean, msg: MapResult): MapResult.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: MapResult, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): MapResult;
+    static deserializeBinaryFromReader(message: MapResult, reader: jspb.BinaryReader): MapResult;
+}
+
+export namespace MapResult {
+    export type AsObject = {
+        key: string,
+        value: string,
+    }
+}
+
+export class DatasResponse extends jspb.Message { 
+    getError(): string;
+    setError(value: string): void;
+
+    clearResultList(): void;
+    getResultList(): Array<MapResult>;
+    setResultList(value: Array<MapResult>): void;
+    addResult(value?: MapResult, index?: number): MapResult;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): DatasResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: DatasResponse): DatasResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: DatasResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): DatasResponse;
+    static deserializeBinaryFromReader(message: DatasResponse, reader: jspb.BinaryReader): DatasResponse;
+}
+
+export namespace DatasResponse {
+    export type AsObject = {
+        error: string,
+        resultList: Array<MapResult.AsObject>,
+    }
+}
