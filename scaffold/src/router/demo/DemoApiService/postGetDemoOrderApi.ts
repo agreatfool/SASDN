3d03250541cd32e857124830e48f53b6ff2499a2
iT1000s <cobrasyq888@gmail.com>
2018-02-28 09:18:48 +0800

    add comment demo to scaffold

--- scaffold/src/router/demo/DemoApiService/postGetDemoOrderApi.ts
+++ scaffold/src/router/demo/DemoApiService/postGetDemoOrderApi.ts
@@ -1,6 +1,8 @@
-import { GatewayApiBase, GatewayContext, MiddlewareNext } from 'sasdn';
-import { Order, } from '../../../proto/order/order_pb';
-import { GetDemoOrderRequest, } from '../../../proto/demo/demo_pb';
+import * as Mock from 'mockjs';
+import {GatewayApiBase, GatewayContext, MiddlewareNext, joi as LibJoi} from 'sasdn';
+import { PbJoi } from '../../../lib/PbJoi';
+import {Order, } from '../../../proto/order/order_pb';
+import {GetDemoOrderRequest, } from '../../../proto/demo/demo_pb';
 import { OrderLogic } from '../../../logic/gateway/OrderLogic';
 
 interface RequestParams {
@@ -14,21 +16,28 @@ class PostGetDemoOrderApi extends GatewayApiBase {
     this.uri = '/v1/getDemoOrder';
     this.type = 'application/json; charset=utf-8';
     this.schemaDefObj = {
-      body: {
-        type: 'object',
-        required: true,
-        schema: {
-          order_id: {
-            type: 'string',
-            required: false,
-          },
-        },
-      },
+      body: LibJoi.object().keys({
+        orderId: PbJoi.vInt64.base().required(),
+      })
     };
   }
 
   public async handle(ctx: GatewayContext, next: MiddlewareNext, params: RequestParams): Promise<Order.AsObject> {
-    const response = await OrderLogic.getOrder(ctx, next, params);
+    const order: Order = await OrderLogic.getOrder(ctx, next, params);
+    return Promise.resolve(order.toObject());
+  }
+
+  public async handleMock(ctx: GatewayContext, next: MiddlewareNext, params: RequestParams): Promise<Order.AsObject> {
+    const response = new Order();
+    response.setOrderId(Mock.Random.natural());
+    response.setUserId(Mock.Random.string('symbol', 5, 10));
+    response.setPrice(Mock.Random.string('symbol', 5, 10));
+    response.setIspayed(Mock.Random.boolean());
+
+    // Map Items
+    response.getItemsMap()
+      .set(Mock.Random.string('lower', 5, 10), Mock.Random.string('lower', 5, 10));
+
     return Promise.resolve(response.toObject());
   }
 }
